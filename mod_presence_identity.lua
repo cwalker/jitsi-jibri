de0d89ae3bcd5abde9d1fab5fd51bb07e0e6a009
cwalker <arafikov@gmail.com>
2022-08-01 17:28:10 +0400

    Update mod_presence_identity.lua

--- mod_presence_identity.lua
+++ mod_presence_identity.lua
@@ -1,7 +1,7 @@
 local stanza = require "util.stanza";
 local update_presence_identity = module:require "util".update_presence_identity;
 
--- For all received presence messages, if the jitsi_meet_context_(user|email)
+-- For all received presence messages, if the jitsi_meet_context_(user|group)
 -- values are set in the session, then insert them into the presence messages
 -- for that session.
 function on_message(event)
@@ -11,7 +11,7 @@ function on_message(event)
           update_presence_identity(
               event.stanza,
               event.origin.jitsi_meet_context_user,
-              event.origin.jitsi_meet_context_email["email"]
+              event.origin.jitsi_meet_context_user["email"]
           );
 
       end
