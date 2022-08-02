0a530fd32444f88b97ad88e3efa7e40f214a398e
cwalker <arafikov@gmail.com>
2022-08-02 15:34:41 +0400

    Update mod_whitelist_jicofo.lua

--- mod_whitelist_jicofo.lua
+++ mod_whitelist_jicofo.lua
@@ -6,7 +6,7 @@ local room, stanza = event.room, event.stanza;
 local user, domain, res = jid.split(event.stanza.attr.from);
 log(“info”, “--------------> user %s domain %s res %s pass %s”, tostring(user),tostring(domain),tostring(res),tostring(room:get_password()));
 
-if user==‘focus’ and domain==‘auth.meet.jitsi’ then
+if user==‘focus’ and domain==‘mauth.jitsi.sesh.ru’ then
 local join = stanza:get_child(“x”, MUC_NS);
 join:tag(“password”, { xmlns = MUC_NS }):text(room:get_password());
 end;
